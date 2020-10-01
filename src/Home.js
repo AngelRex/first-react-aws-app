1d8c4feee042cf9942cbd57dfc8ff324e0da2d2d
Angel Rex <angelrex.2019@gmail.com>
2020-10-01 19:31:26 -0400

    first commit

---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div>
+      <img src="cloud1.jpg" alt="" className="home-pic" />
+    </div>
+  );
+}
+export default Home;
